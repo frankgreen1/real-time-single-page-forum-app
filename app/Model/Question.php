7817775067a4264a64dae76e60805e7c2d0b4852
greenfrank1 <greenfrank010@gmail.com>
2020-09-09 11:35:39 +0100

    relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,10 +2,32 @@
 
 namespace App\Model;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
     use HasFactory;
+
+
+    public function user()
+    {
+      return   $this->belongsTo(User::class);
+    }
+
+   public function replies()
+   {
+       return $this->hasMany(Reply::class);
+   }
+
+   public function category()
+   {
+       return $this->belongsTo(Category::class);
+   }
+
+
+
+
+
 }
